acd3dea7b72a01e0dba43a7a27506b6c8f67fa62
Fadli Saad <fadlisaad@gmail.com>
2023-07-03 10:18:23 +0800

    Cannot read config file

--- php/agency-list.php
+++ php/agency-list.php
@@ -1,18 +1,7 @@
 <?php
 require ('conn.php');
 
-$config_filename = 'config.json';
-if (!file_exists($config_filename)) {
-    throw new Exception("Can't find ".$config_filename);
-}
-$config = json_decode(file_get_contents($config_filename), true);
-
-if($config['fpx']['environment'] == 'Staging'){
-	$stm = $pdo->query("SELECT * FROM agencies WHERE code LIKE '%STG'");
-} else {
-	$stm = $pdo->query("SELECT * FROM agencies");
-}
-
+$stm = $pdo->query("SELECT * FROM agencies");
 $rows = $stm->fetchAll(PDO::FETCH_ASSOC);
 
 $list = NULL;
